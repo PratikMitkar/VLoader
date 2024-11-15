6bb606d6b71b032fbfa98b32740c8ffcb821d35b
PratikMitkar <pratikmitkar12@gmail.com>
2024-11-15 15:38:12 +0530

    download is added for windows

--- src/components/DownloadModal.jsx
+++ src/components/DownloadModal.jsx
@@ -5,20 +5,22 @@ import { FaWindows, FaApple, FaLinux } from 'react-icons/fa'
 export default function DownloadModal({ isOpen, onClose }) {
   if (!isOpen) return null
 
+  const windowsDownloadUrl = "https://github.com/PratikMitkar/VLoader-python/releases/download/v0.1/VLoader-v0.1-windows.zip"
+
   return (
     <div className={styles.modal}>
       <div className={styles.modalContent}>
         <h2>Download VLoader</h2>
         <p>Click the button below to download VLoader. After downloading, unzip the file to start using the software.</p>
         <div className={styles.downloadButtons}>
-          <a href="#" className={styles.downloadButton}>
+          <a href={windowsDownloadUrl} className={styles.downloadButton} download>
             <FaWindows /> Download for Windows
           </a>
-          <a href="#" className={styles.downloadButton}>
-            <FaApple /> Download for Mac
+          <a href="#" className={styles.downloadButton} onClick={(e) => e.preventDefault()}>
+            <FaApple /> Coming Soon for Mac
           </a>
-          <a href="#" className={styles.downloadButton}>
-            <FaLinux /> Download for Linux
+          <a href="#" className={styles.downloadButton} onClick={(e) => e.preventDefault()}>
+            <FaLinux /> Coming Soon for Linux
           </a>
         </div>
         <button className={styles.closeModal} onClick={onClose}>&times;</button>
